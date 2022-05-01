125c1cdfc774d781cfe38ec8c3974eede60aeaac
ibrahim <ibrahimyildirimfr@gmail.com>
2022-05-02 02:17:57 +0300

    delete wrong dir

--- LiscovSubstitution/AlanHesapla.java
+++
@@ -1,9 +0,0 @@
-package LSP;
-
-public interface AlanHesapla {
-	//Ara sınıf olusturmak yerine alan icin bir interface de olusturabilirdik.
-	//egerr cok farklı ozellikler varsa interface kullanılır
-	//istisna durumlar varsa ara sınıf olusturulabilir.
-	public double alan();
-	
-}
